12caf991bf393df0c3bdf4b09a774093ae5f517a
soniaarl <59911419+soniaarl@users.noreply.github.com>
2020-06-14 00:42:33 -0700

    google analytics tracker

--- header.php
+++ header.php
@@ -19,6 +19,16 @@
 	<link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
 
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125324556-3"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-125324556-3');
+	</script>
+	
 	<?php wp_head(); ?>
 </head>
 
